3f9e36d76c67d2fa4e239d635723a8b7e851b852
Vasmitha <mvasmitha07@gmail.com>
2026-05-31 15:56:42 +0530

    Add temperature in celsius to get activity suggestions

---
+++ Day 2/temp_advisor.py
@@ -0,0 +1,16 @@
+# Smart Temperature Advisor
+
+# Reads temperature in C
+temp = int(input("Enter the temperature in celsius: "))
+
+# Activity advice
+if temp < 0:
+    print("Freezing! Stay indoors and wear heavy clothing.")
+elif 0 <= temp <= 15:
+    print("Cold. A jacket is recommended.")
+elif 16 <= temp <= 25:
+    print("Pleasant weather! Great for outdoor activities.")
+elif 26 <= temp <= 35:
+    print("Hot. Stay hydrated and use sunscreen.")
+else:
+    print("Extreme heat! Avoid going outside.")
